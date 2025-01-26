4e989ed3045fe923eb4e18d1242bd95937b0a874
Roman Koshchei <romankoshchei@gmail.com>
2025-01-26 20:31:51 +0200

    nic

--- src/telegram.rs
+++ src/telegram.rs
@@ -1,14 +1,19 @@
-use alloc::format;
-use embedded_io_async::{Read, Write};
-use log::info;
-use reqwless::{client::HttpResource, headers::ContentType, request::RequestBuilder as _};
+use alloc::{format, string::ToString};
+use embedded_nal_async::{Dns, TcpConnect};
+// use log;
+use reqwless::{
+    client::{HttpClient, HttpResource},
+    headers::ContentType,
+    request::RequestBuilder as _, response::StatusCode,
+};
 use serde_json_core as _;
 
-pub struct Client<'a, C>
+pub struct Client<'a, T, D>
 where
-    C: Read + Write,
+    T: TcpConnect + 'a,
+    D: Dns + 'a,
 {
-    http: reqwless::client::HttpResource<'a, C>,
+    client: HttpClient<'a, T, D>,
     bot_token: &'a str,
     chat_id: &'a str,
     response_buffer: [u8; 8196],
@@ -16,20 +21,55 @@ where
 
 pub const HOSTNAME: &str = "https://api.telegram.org";
 
-impl<'a, C> Client<'a, C>
+impl<'a, T, D> Client<'a, T, D>
 where
-    C: Read + Write,
+    T: TcpConnect + 'a,
+    D: Dns + 'a,
 {
-    pub fn new(http: HttpResource<'a, C>, bot_token: &'a str, chat_id: &'a str) -> Self {
+    pub fn new(
+        // for http
+        tcp: &'a T,
+        dns: &'a D,
+        tls: reqwless::client::TlsConfig<'a>,
+
+        // secrets
+        bot_token: &'a str,
+        chat_id: &'a str,
+    ) -> Self {
         Self {
-            http,
+            client: HttpClient::new_with_tls(tcp, dns, tls),
             bot_token,
             chat_id,
             response_buffer: [0; 8196],
         }
     }
 
-    pub async fn send_message(&mut self, text: &str, is_html: bool) -> bool {
+    // only with Polonius
+    // async fn resource(
+    //     client: &'a mut HttpClient<'a, T, D>,
+    // ) -> Option<HttpResource<'a, T::Connection<'a>>> {
+    //     const MAX_ATTEMPTS: u8 = 8;
+    //     let mut attempt: u8 = 0;
+    //     loop {
+    //         attempt += 1;
+    //         match client.resource(HOSTNAME).await {
+    //             Ok(res) => {
+    //                 return Some(res);
+    //             }
+    //             Err(_) => {
+    //                 if attempt >= MAX_ATTEMPTS {
+    //                     return None;
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+
+    pub async fn send_message(
+        &mut self,
+        text: &str,
+        is_html: bool,
+    ) -> Result<(), SendMessageError> {
         let parse_mode = if is_html { "HTML" } else { "MarkdownV2" };
         let path = format!("/bot{}/sendMessage", self.bot_token);
 
@@ -43,38 +83,63 @@ where
             },
             &mut body_buffer,
         )
-        .expect("Body Buffer is too small"); // TODO: rework without panic
-                                             // info!("TG: send_message request body size: {}", size);
+        .map_err(|_| SendMessageError::TooSmallBodyBuffer)?;
+        // info!("TG: send_message request body size: {}", size);
 
-        let res = self
-            .http
+        let mut resource = self
+            .client
+            .resource(HOSTNAME)
+            .await
+            .map_err(SendMessageError::ReqwlessError)?;
+
+        let response = resource
             .post(&path)
             .body(&body_buffer[..])
             .content_type(ContentType::ApplicationJson)
             .send(&mut self.response_buffer)
-            .await;
+            .await
+            .map_err(SendMessageError::ReqwlessError)?;
         // log::info!("TG: send_message response");
 
-        res.is_ok_and(|x| x.status.is_successful())
+        if response.status.is_successful() {
+            Ok(())
+        } else {
+            let status = response.status;
+            let body = response.body().read_to_end().await.unwrap();
+            let str_body = alloc::str::from_utf8(&body).unwrap();
+            log::error!("{}", str_body);
+
+            Err(SendMessageError::StatusCodeIsNotSuccessful(status))
+        }
     }
 
-    pub async fn get_updates(&mut self, offset: i64) -> Option<TelegramUpdates> {
+    pub async fn get_updates(&mut self, offset: i64) -> Result<TelegramUpdates, GetUpdatesError> {
         let path = format!("/bot{}/getUpdates?offset={}", self.bot_token, offset);
 
-        let response = self
-            .http
+        let mut resource = self
+            .client
+            .resource(HOSTNAME)
+            .await
+            .map_err(GetUpdatesError::ReqwlessError)?;
+
+        let response = resource
             .get(&path)
             .send(&mut self.response_buffer)
             .await
-            .ok()?;
+            .map_err(GetUpdatesError::ReqwlessError)?;
         // log::info!("TG: get_updates got response");
 
-        let body = response.body().read_to_end().await.ok()?;
+        let body = response
+            .body()
+            .read_to_end()
+            .await
+            .map_err(GetUpdatesError::ReqwlessError)?;
         // log::info!("TG: get_updates response body size: {}", body.len());
 
         let serialized = serde_json_core::from_slice::<TelegramUpdates>(body)
-            .expect("TG: get_updates serialize failed");
-        Some(serialized.0)
+            .map_err(|_| GetUpdatesError::DeserializationFailed)?;
+
+        Ok(serialized.0)
     }
 }
 
@@ -84,6 +149,17 @@ struct SendMessageBody<'a> {
     text: &'a str,
     protect_content: bool,
     parse_mode: &'a str,
+}
+
+pub enum SendMessageError {
+    TooSmallBodyBuffer,
+    StatusCodeIsNotSuccessful(StatusCode),
+    ReqwlessError(reqwless::Error),
+}
+
+pub enum GetUpdatesError {
+    DeserializationFailed,
+    ReqwlessError(reqwless::Error),
 }
 
 #[derive(serde::Deserialize)]
